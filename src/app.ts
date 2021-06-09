721ceed5b1fffa9fb1ca599eab4be7c6e49fda30
Peter Olayinka <peterolayinka97@gmail.com>
2021-06-09 12:22:42 +0100

    concluded task

---
+++ src/app.ts
@@ -0,0 +1,215 @@
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: process.env.REGION ? process.env.REGION : "us-east-1",
+});
+
+function getKeyName(folder, filename) {
+  return folder + "/" + filename;
+}
+
+interface Signature {
+  timestamp: "string";
+  token: "string";
+  signature: "string";
+}
+
+interface EventData {
+  event: string,
+  timestamp: "string";
+  id: "string";
+}
+
+enum StorageOptions {
+  "s3",
+  "drive",
+  "dropbox",
+}
+
+enum NotificationeOptions {
+  "sns",
+  "relic",
+  "pagerDuty",
+}
+
+class WebhookProcessor {
+  request: any;
+  event: EventData;
+  signature: Signature;
+  action: string;
+
+  constructor(
+    request: any,
+  ) {
+    this.request = request;
+    let parsedBody = JSON.parse(this.request.body);
+
+    this.event = parsedBody["event-data"];
+    this.signature = parsedBody.signature;
+    this.action = `email ${this.event.event}`;
+  }
+
+  validateEnvironmentVariables() {
+    if (!process.env.SIGNING_KEY) {
+      throw new Error("Please provide a signing key")
+    }
+    if (!process.env.NOTIFICATION_SERVICE) {
+      throw new Error("Please specify notification service")
+    }
+    if (!process.env.STORAGE_SERVICE) {
+      throw new Error("Please specify storage service")
+    }
+  }
+
+  processWebHook() {
+    let body: string;
+    let statusCode = "200";
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    this.validateEnvironmentVariables()
+
+    if (
+      this.isVerifiedSender(
+        process.env.SIGNING_KEY,
+        this.signature.timestamp,
+        this.signature.token,
+        this.signature.signature
+      )
+    ) {
+      try {
+        switch (this.request.httpMethod) {
+          case "POST":
+            body = this.sendNotification(process.env.NOTIFICATION_SERVICE);
+            body += this.storeEventLog(process.env.STORAGE_SERVICE);
+
+            break;
+          default:
+            throw new Error(`Unsupported method "${this.request.httpMethod}"`);
+        }
+      } catch (err) {
+        statusCode = "400";
+        body = err.message;
+      } finally {
+        body = JSON.stringify(body);
+      }
+    } else {
+      statusCode = "400";
+      body = "Unauthorised request";
+    }
+
+    return {
+      statusCode,
+      body,
+      headers,
+    };
+  }
+
+  isVerifiedSender(
+    signingKey: string,
+    timestamp: string,
+    token: string,
+    signature: string
+  ) {
+    const crypto = require("crypto");
+    const encodedToken = crypto
+      .createHmac("sha256", signingKey)
+      .update(timestamp.concat(token))
+      .digest("hex");
+
+    return encodedToken === signature;
+  }
+
+  processSNS() {
+    if (!process.env.SNS_ARN) {
+      throw new Error("Please specify SNS ARN")
+    }
+
+    let sns = new AWS.SNS({ apiVersion: "2010-03-31" });
+    let snsParams = {
+      Message: this.action,
+      Subject: "Mailgun Webhook notification",
+      TopicArn: process.env.SNS_ARN,
+    };
+
+    // Create promise and SNS service object
+    var snsResult = sns.publish(snsParams).promise();
+
+    snsResult
+      .then(function (data) {
+        console.log(
+          `Message ${snsParams.Message} sent to the topic ${snsParams.TopicArn}`
+        );
+        console.log("MessageID is " + data.MessageId);
+      })
+      .catch(function (err) {
+        console.error(err, err.stack);
+      });
+  }
+
+  processS3() {
+    if (!process.env.S3_BUCKET_NAME) {
+      throw new Error("Please specify S3 bucket name")
+    }
+
+    let s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+    let s3Content = {
+      Provider: "Mailgun",
+      timestamp: this.event.timestamp,
+      type: this.action,
+    };
+
+    var bucketName = "receeve-mailgun";
+    let keyName = `${this.event.id}.txt`
+
+    var objectParams = {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: keyName,
+      Body: JSON.stringify(s3Content, null, 2),
+    };
+
+    // Create object upload promise
+    var uploadPromise = s3.putObject(objectParams).promise();
+    uploadPromise.then(function (data) {
+      console.log(
+        "Successfully uploaded data to " + bucketName + "/" + keyName
+      );
+    })
+      .catch(function (err) {
+        console.error(err, err.stack);
+      });
+  }
+
+  sendNotification(service: string) {
+    switch (service) {
+      case "sns":
+        this.processSNS()
+        break;
+      default:
+        throw new Error(`Please provide a service`);
+    }
+    return "Notification sent successfully.";
+  }
+
+  storeEventLog(service: string) {
+    switch (service) {
+      case "s3":
+        this.processS3()
+        break;
+      default:
+        throw new Error(`Please provide a service`);
+    }
+    return "Event stored successfully.";
+  }
+}
+
+const handler = async (event, context) => {
+  console.log("Received event:", JSON.stringify(event, null, 2));
+  const mailgun = new WebhookProcessor(event);
+  const processResponse = mailgun.processWebHook();
+  console.log(processResponse);
+  return processResponse
+};
+
+exports.handler = handler
---
+++ startup.sh
@@ -0,0 +1,5 @@
+export NOTIFICATION_SERVICE=sns
+export S3_BUCKET_NAME=receeve-mailgun
+export SIGNING_KEY=password
+export SNS_ARN=arn:aws:sns:us-east-1:302527607125:Receeve-SNS-Topic
+export STORAGE_SERVICE=s3
