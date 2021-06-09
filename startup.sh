788f65a8db3a744557328418b4ec30ac42fb7d2e
Peter Olayinka <peterolayinka97@gmail.com>
2021-06-09 14:10:22 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 export NOTIFICATION_SERVICE=sns
 export S3_BUCKET_NAME=receeve-mailgun
 export SIGNING_KEY=password
-export SNS_ARN=arn:aws:sns:us-east-1:302527607125:Receeve-SNS-Topic
+export SNS_ARN=arn:aws:sns:us-east-1:1234567890:Receeve-SNS-Topic
 export STORAGE_SERVICE=s3
